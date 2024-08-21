851a8b0771320edb92bb8944a1dd535b0fa2e901
Henry Walters <henrywalters20@gmail.com>
2024-08-20 21:06:41 -0400

    remove release folder

--- release/v0/TheRunnerWeb/server.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const app = express()
-const port = 1337
-
-app.use(express.static('./'));
-
-app.listen(port, () => {
-    console.log(`Demo app listening at http://localhost:${port}`)
-})
